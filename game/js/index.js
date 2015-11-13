36a8f31c83454c0204e2dc88b72fed00805ec222
roberj1 <roberj1@wis.edu.hk>
2015-11-13 09:59:12 +0800

    game

---
+++ game/js/index.js
@@ -0,0 +1,77 @@
+function gamebutton(){
+
+var q1 = prompt("What is 5x7");
+if (q1 ==="35")
+{
+  alert("Great job");
+} 
+
+else
+{
+  alert("WRONG sorry try again!");
+}
+
+var q2 = prompt("What is 7x9");
+if (q2 ==="63")
+{
+  alert("Great job");
+} else
+{
+  alert("WRONG sorry try again!");
+}
+
+var q3 = prompt("If a trian is going 50 miles an hour and a nother train is going 50 km/h what is 5x5?");
+if (q3 === "25")
+{
+  alert("Great job");
+} else
+{
+  alert("WRONG sorry try again!");
+}
+
+var q4 = prompt("What is 9x8");
+if (q3 ==="75")
+{
+  alert("Great job");
+} else
+{
+  alert("WRONG sorry try again!");
+}
+
+var q5 = prompt("4x6")
+if(q5 ==="24")
+{
+  alert("Great job");
+} else
+{
+  alert("WRONG sorry try again!");
+}
+
+var q6 = prompt("12x9")
+if(q6 ==="108")
+{
+  alert("Great job");
+} else
+{
+  alert("WRONG sorry try again!");
+}
+
+var q7 = prompt("6x8")
+if(q7 = 48)
+{
+  alert("Great job");
+} else
+{
+  alert("WRONG sorry try again!");
+}
+
+var q8 = prompt("2x0")
+if(q8 === "0")
+{
+  alert("Great job");
+} else
+{
+  alert("WRONG sorry try again!");
+}
+  
+}
